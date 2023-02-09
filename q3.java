2fde127609ac62407781c4b3e9e1847b84894c64
MrWilco <87799625+MrWilco@users.noreply.github.com>
2023-02-09 14:15:07 -0500

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class q3 {
+
+public static void main(String[] args) {    
+        double total = 0;
+        boolean isPeas = false;
+        Integer amount = Integer.parseInt(JOptionPane.showInputDialog("How many items do you want? "));
+        String items[] = new String[amount];
+        double prices[] = new double[amount];
+        for (int num = 1; num < amount + 1; num = num + 1) { 
+            items[num-1] = JOptionPane.showInputDialog("Enter Item " + num + ": ");
+            prices[num-1] = Double.parseDouble(JOptionPane.showInputDialog("Enter price for item " + num + ": "));
+            total = total + prices[num-1];
+            if (items[num-1].equals("peas")) {
+                isPeas = true;
+            };
+            System.out.print(items[amount - num-1] + ", ");
+        }
+        total = total / amount;
+        if (isPeas == true) {
+            System.out.print("Your total is $" + Math.round(total));    
+        }
+        else {
+            System.out.print("No average ouput");
+        } 
+    }
+}
